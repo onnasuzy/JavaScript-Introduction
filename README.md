b41550efe4b3e588c664f3221394bffe1075a965
onnasuzy <onnasuzyd@gmail.com>
2022-06-24 18:47:13 +0100

    first JS code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JavaScript-Introduction
+This is an I4GxZuri Task on JavaScript to ptint out name, height and country on screen.
---
+++ firstscript.js
@@ -0,0 +1,5 @@
+let myName= 'My name is Ogbonna Chioma Suzy,';
+let height= 'I am 158cm tall,';
+let country= 'I am from Nigeria.';
+
+alert(myName + "\n" + height + "\n" + country);
